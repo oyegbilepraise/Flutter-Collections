e4cbd6faf4026c314a9c5a5d828e2204d41bc819
Oyegbile Praise <oluwadhammieh@gmail.com>
2023-02-11 15:52:29 +0100

    Wallet App Completed

--- lib/Inspiration App/inspiration_home_page.dart
+++ lib/Inspiration App/inspiration_home_page.dart
@@ -37,149 +37,151 @@ class _InspirationHomePageState extends State<InspirationHomePage> {
           print(index);
         },
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(30)),
-              ),
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Find Your',
-                    style: TextStyle(color: Colors.black87, fontSize: 25),
-                  ),
-                  const SizedBox(height: 5),
-                  const Text(
-                    'Inspiration',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(30)),
+                ),
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Find Your',
+                      style: TextStyle(color: Colors.black87, fontSize: 25),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: const Color.fromRGBO(244, 243, 243, 1),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search, color: Colors.black87),
-                        hintText: 'Search you"re looking for',
+                    const SizedBox(height: 5),
+                    const Text(
+                      'Inspiration',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Promo Today',
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    height: 200,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        PromoCard('assets/icons/white.jpeg'),
-                        PromoCard('assets/images/carlo.jpeg'),
-                        PromoCard('assets/images/child.jpeg'),
-                        PromoCard('assets/images/mug.jpeg'),
-                        PromoCard('assets/images/senoka.jpeg'),
-                      ],
-                    ),
-                  ),
-                  // 0051351177
-                  const SizedBox(height: 20.0),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage('assets/images/child.jpeg'),
-                      ),
-                    ),
-                    child: Container(
+                    const SizedBox(height: 20),
+                    Container(
+                      padding: const EdgeInsets.all(5),
                       decoration: BoxDecoration(
+                        color: const Color.fromRGBO(244, 243, 243, 1),
                         borderRadius: BorderRadius.circular(20),
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomRight,
-                          stops: const [0.1, 0.9],
-                          colors: [
-                            Colors.black.withOpacity(.8),
-                            Colors.black.withOpacity(.1)
-                          ],
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: Colors.black87),
+                          hintText: 'Search you"re looking for',
                         ),
                       ),
-                      child: const Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Text(
-                            'Best Design',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Promo Today',
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          PromoCard('assets/icons/white.jpeg'),
+                          PromoCard('assets/images/carlo.jpeg'),
+                          PromoCard('assets/images/child.jpeg'),
+                          PromoCard('assets/images/mug.jpeg'),
+                          PromoCard('assets/images/senoka.jpeg'),
+                        ],
+                      ),
+                    ),
+                    // 0051351177
+                    const SizedBox(height: 20.0),
+                    Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/images/child.jpeg'),
+                        ),
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomRight,
+                            stops: const [0.1, 0.9],
+                            colors: [
+                              Colors.black.withOpacity(.8),
+                              Colors.black.withOpacity(.1)
+                            ],
+                          ),
+                        ),
+                        child: const Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text(
+                              'Best Design',
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage('assets/icons/white.jpeg'),
-                      ),
-                    ),
-                    child: Container(
+                    const SizedBox(height: 20.0),
+                    Container(
+                      height: 100,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomRight,
-                          stops: const [0.1, 0.9],
-                          colors: [
-                            Colors.black.withOpacity(.8),
-                            Colors.black.withOpacity(.1)
-                          ],
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/icons/white.jpeg'),
                         ),
                       ),
-                      child: const Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Text(
-                            'Best Design',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomRight,
+                            stops: const [0.1, 0.9],
+                            colors: [
+                              Colors.black.withOpacity(.8),
+                              Colors.black.withOpacity(.1)
+                            ],
+                          ),
+                        ),
+                        child: const Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text(
+                              'Best Design',
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
